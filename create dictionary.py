62f322bc4eb97d893963c2d371d48d7370767656
Vinayak sood <113632279+Vinayak26809@users.noreply.github.com>
2023-10-28 11:26:28 +0530

    Add files via upload

---
+++ create dictionary.py
@@ -0,0 +1,17 @@
+dict1={}
+print(" Empty dictionaryi:")
+print(dict1)
+
+# Creating a Dictionary  
+# using the dict() method  
+
+dict1 =dict({1:'Hello',2:'Hey',3:'Hi'})
+print("\n create dictionary by using dict() method ")
+print(dict1)
+
+# Creating a Dictionary  
+# using the dict() method  
+
+dict2 =dict ({('Josehp',90014),('Kartikay',90015)})
+print("\n dictinary with each item as a pair")
+print(dict2)
